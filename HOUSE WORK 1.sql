d28d76e00f5f3e1b9b5317c816ba77e579b4e6fa
Ikay <Myexcel57@gmail.com>
2025-04-12 20:11:13 +0100

    UPDATE SQL FILE

--- HOUSE WORK 1.sql
+++ HOUSE WORK 1.sql
@@ -1,11 +1,11 @@
 -- HOUSE WORK
 -- use a table named employees with the following structure:
 -- EMLOYEE ID   FIRST NAME   LAST NAME     DEPARTMENT   SALARY  HIRE DATE
--- 1            JOHN         DOE           HR           50000   2019-04-23
--- 2            JANE         SMITH         IT           70000   2020-08-15
--- 3            ALICE        JOHNSON       IT           80000   2018-11-12
--- 4            BOB          BROWN         FINANCE      45000   2021-01-10
--- 5            CHARLIE      DAVIS         HR           55000   2022-05-05
+-- 1            JOHN         EMEKA           HR           50000   2019-04-23
+-- 2            JANE         ADE             IT           70000   2020-08-15
+-- 3            CHIDIMA     JOHNSON          IT           80000   2018-11-12
+-- 4            ADEYEMI     BRIGHT           FINANCE      45000   2021-01-10
+-- 5            DANIEL     NWACHUKWU         HR           55000   2022-05-05
 
 # TASK:
 -- Basic Filtering:
@@ -36,11 +36,7 @@
 
 -- Find employees in the IT department OR employees who were hired after January 1, 2021.
 
-
-
-
--- ANSWER ONE. FINDING ALL EMPLOYEES IN THE IT DEPARTMENT
- --     SOLUTION
+-- 8 Write a query that shows all employees who Work in the IT department AND Were hired before 2020-01-01 OR Have a salary greater than 75000`
       
 USE db_order;
 
@@ -54,12 +50,78 @@ Hire_Date DATE
 
 );
 
-INSERT INTO tbl_employees VALUES(1,'John','Doe','HR',50000,'2019-04-23');
-INSERT INTO tbl_employees VALUES(2,'Jane','Smith','IT',70000,'2020-08-15');
-INSERT INTO tbl_employees VALUES(3,'Alice','Johnson','IT',80000,'2018-11-12');
-INSERT INTO tbl_employees VALUES(4,'Bob','Brown','Finance',45000,'2021-01-10');
-INSERT INTO tbl_employees VALUES(5,'Charlie','Davis','HR',55000,'2022-05-05');
+INSERT INTO tbl_employees VALUES(1,'John','Emeka','HR',50000,'2019-04-23');
+INSERT INTO tbl_employees VALUES(2,'Jane','Ade','IT',70000,'2020-08-15');
+INSERT INTO tbl_employees VALUES(3,'Chidima','Johnson','IT',80000,'2018-11-12');
+INSERT INTO tbl_employees VALUES(4,'Adeyemi','Bright','Finance',45000,'2021-01-10');
+INSERT INTO tbl_employees VALUES(5,'Daniel','Nwachukwu','HR',55000,'2022-05-05');
+
+SELECT * FROM tbl_employees;
+
+
+-- ANSWER 1. FINDING ALL EMPLOYEES IN THE IT DEPARTMENT
+ --     SOLUTION
 
 SELECT * FROM tbl_employees
-WHERE Department ='IT'
+WHERE Department ='IT' 
+
+;
+
+
+-- ANSWER 2. Find employees in the HR department who earn more than 50000.
+        -- SOLUTION
+
+SELECT * FROM tbl_employees
+WHERE Department ='HR' AND Salary > 50000
+;
+
+
+
+-- ANSWER 3. Retrieve employees whose first name starts with 'J'.
+    -- SOLUTION
+
+SELECT * FROM tbl_employees
+WHERE First_Name like 'J%' ;
+
+-- ANSWER 4. List all employees who were hired before January 1, 2020.-- List all employees who were hired before January 1, 2020.
+       -- SOLUTION
+       
+SELECT * FROM tbl_employees
+WHERE Hire_Date < '2020-01-01'
+;
+
+-- ANSWER 5. Get employees whose salaries are between 45000 and 70000.
+         -- SOLUTION
+  
+SELECT * FROM tbl_employees
+WHERE Salary BETWEEN 45000 AND 70000
+;
+         
+
+-- ANSWER 6. Select employees who work in either HR or Finance departments.
+        -- SOLUTION
+        
+SELECT * FROM tbl_employees
+WHERE  Department  IN  ('HR' ,'FINANCE')
+
+;
+
+-- ANSWER 7. Find employees in the IT department OR employees who were hired after January 1, 2021.
+         -- SOLUTION
+         
+SELECT * FROM tbl_employees
+WHERE  Department  = 'IT'OR  Hire_Date > '2021-01-01' 
+;
+
+-- ANSWER 8 .  Write a query that shows all employees who Work in the IT department AND Were hired before 2020-01-01 OR Have a salary greater than 75000
+     -- SOLUTION
+     
+SELECT * FROM tbl_employees
+WHERE Department =  'IT' AND (Hire_Date <'2020-01-01' OR Salary > 75000)
+;
+
+
+
+
+
 
